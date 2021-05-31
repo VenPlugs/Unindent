dcf2be10da2aba98f5ac5197dfad3599ac7e78b8
Vendicated <vendicated@riseup.net>
2021-05-31 03:09:06 +0200

    Use regex instead of manual parsing

--- index.js
+++ index.js
@@ -51,53 +51,56 @@ module.exports = class Unindent extends Plugin {
   }
 
   unindent(str, firstLineIdx) {
-    const lines = str.split("\n");
-    // First line sometimes (?) has no indent (thanks discord), so use the second line as reference in these cases
-    let choseSecondLine = false;
-    let firstIndentedLine;
-    if (indents.indexOf(lines[firstLineIdx].charAt(0)) === -1) {
-      firstIndentedLine = lines[firstLineIdx + 1];
-      choseSecondLine = true;
-    } else {
-      firstIndentedLine = lines[firstLineIdx];
-    }
-
-    if (!firstIndentedLine) return str;
-
-    let indentAmount = 0;
-    for (const char of firstIndentedLine) {
-      const idx = indents.indexOf(char);
-      if (idx === -1) break;
-      indentAmount += idx;
-    }
-
-    // FIXME (Find less hacky solution): If indent amount is based on second line, try to figure out it is in block (thus indented further) and if so leave additional indent
-    if (
-      choseSecondLine &&
-      (lines[firstLineIdx].includes("{") ||
-        lines[firstLineIdx].endsWith(":") ||
-        lines[firstLineIdx].endsWith("; then") ||
-        firstIndentedLine.indexOf("{") === indentAmount + 1)
-    )
-      indentAmount -= 2;
-
-    if (indentAmount <= 0) return str;
-
-    for (let i = firstLineIdx; i < lines.length; i++) {
-      const line = lines[i];
-      let unindentedIdx = 0;
-      for (let j = 0, unindentedAmount = 0; j < line.length; j++) {
-        const char = line[j];
-        const idx = indents.indexOf(char);
-        if (idx === -1) break;
-        unindentedIdx++;
-        unindentedAmount += idx;
-        if (unindentedAmount >= indentAmount) break;
-      }
-      lines[i] = line.slice(unindentedIdx);
-    }
-
-    return lines.join("\n");
+    const minIndent = str.match(/^ *(?=\S)/gm)?.reduce((prev, curr) => Math.min(prev, curr.length), Infinity) ?? 0;
+    return str.replace(new RegExp(`^ {${minIndent}}`, "gm"), "");
+    //
+    // const lines = str.split("\n");
+    // // First line sometimes (?) has no indent (thanks discord), so use the second line as reference in these cases
+    // let choseSecondLine = false;
+    // let firstIndentedLine;
+    // if (indents.indexOf(lines[firstLineIdx].charAt(0)) === -1) {
+    // firstIndentedLine = lines[firstLineIdx + 1];
+    // choseSecondLine = true;
+    // } else {
+    // firstIndentedLine = lines[firstLineIdx];
+    // }
+    //
+    // if (!firstIndentedLine) return str;
+    //
+    // let indentAmount = 0;
+    // for (const char of firstIndentedLine) {
+    // const idx = indents.indexOf(char);
+    // if (idx === -1) break;
+    // indentAmount += idx;
+    // }
+    //
+    // // FIXME (Find less hacky solution): If indent amount is based on second line, try to figure out it is in block (thus indented further) and if so leave additional indent
+    // if (
+    // choseSecondLine &&
+    // (lines[firstLineIdx].includes("{") ||
+    // lines[firstLineIdx].endsWith(":") ||
+    // lines[firstLineIdx].endsWith("; then") ||
+    // firstIndentedLine.indexOf("{") === indentAmount + 1)
+    // )
+    // indentAmount -= 2;
+    //
+    // if (indentAmount <= 0) return str;
+    //
+    // for (let i = firstLineIdx; i < lines.length; i++) {
+    // const line = lines[i];
+    // let unindentedIdx = 0;
+    // for (let j = 0, unindentedAmount = 0; j < line.length; j++) {
+    // const char = line[j];
+    // const idx = indents.indexOf(char);
+    // if (idx === -1) break;
+    // unindentedIdx++;
+    // unindentedAmount += idx;
+    // if (unindentedAmount >= indentAmount) break;
+    // }
+    // lines[i] = line.slice(unindentedIdx);
+    // }
+    //
+    // return lines.join("\n");
   }
 
   pluginWillUnload() {
